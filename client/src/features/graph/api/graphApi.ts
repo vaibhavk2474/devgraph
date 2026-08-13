22a34c89f6680cab48d8a070c241c2f1587ab1db
Vaibhav Kumar <vaibhavk2474@gmail.com>
2026-08-14 00:48:52 +0530

    added env

--- client/src/features/graph/api/graphApi.ts
+++ client/src/features/graph/api/graphApi.ts
@@ -38,7 +38,7 @@ export const graphApi = createApi({
     reducerPath: "graphApi",
 
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api/v1",
+        baseUrl: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
     }),
 
     endpoints: (builder) => ({
